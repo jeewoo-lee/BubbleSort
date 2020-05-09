d7224207c738758ff6ba18083c24b9108f774a58
Jeewoo Lee <jlee@lcs.on.ca>
2020-05-09 15:13:03 +0900

    applied "optimization"

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -22,20 +22,25 @@ for _ in 1...10 {
 print("Unsorted: ")
 print(dataSet)
 
+var swapped = true
+
 // Loop through the entire array "n" times
 // (however many times there are elements in the array)
-for i in 0..<dataSet.count {
+var lastUnsorted = dataSet.count - 1
+while swapped {
+    swapped = false
     //One pass through the array to float the highest number to the end
-    for j in 0..<dataSet.count - 1 {
-        
+    for j in 0..<lastUnsorted {
         //Compare left value to right value
         if dataSet[j] > dataSet[j + 1] {
             // Swap values when left value is more than right value
             let temporaryValue = dataSet[j] // set aside the left value
             dataSet[j] = dataSet[j + 1] //Replace left with right
             dataSet[j+1] = temporaryValue // Replace right with temporary value
+            swapped = true
         }
     }
+    lastUnsorted -= 1
 }
 
 
